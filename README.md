2f417a01f5245f781e627311db7f25b77ae19850
Horacio Sanson <hsanson@gmail.com>
2012-05-31 17:28:27 +0900

    New qpsnr.rb wrapper script.
    This new script works much like the qpsnr binary but instead of outputing SSIM
    and PSNR values to STDOUT it generates a nice HTML page with a flot graph and
    links to the individual frames of each SSIM/PSNR value.
    
    This is good to perform a one by one frame comparison of the encoded files vs
    the original.

--- README.md
+++ README.md
@@ -24,18 +24,73 @@ then compile the source:
 After compilation a `qpsnr` binary should appear in the current directory. You
 can copy it to your bin folder or add the binary path to your PATH environment.
 
- * Compute PSNR from frame 1001 to 1500
+Compute PSNR from frame 1001 to 1500
 
     ./qpsnr -s 1000 -m 500 -r reference_video.avi video1.avi video2.avi video3.avi
 
- * Compute average PSNR in HSI colorspace for the first 500 frames
+Compute average PSNR in HSI colorspace for the first 500 frames
 
     ./qpsnr -m 500 -a avg_psnr --aopts fpa=25:colorspace=hsi -r reference_video.avi video1.avi video2.avi video3.avi
 
- * Compute PSNR in YCbCr colorspace skipping the first 2000 frames and storing each frame in ppm format.
+Compute PSNR in YCbCr colorspace skipping the first 2000 frames and storing each frame in ppm format.
 
     ./qpsnr -I -s 2000 --aopts colorspace=ycbcr -r reference_video.avi video1.avi video2.avi video3.avi
 
- * Same as above but save in jpeg format instead of ppm
+Same as above but save in jpeg format instead of ppm
 
     ./qpsnr -J -s 2000 --aopts colorspace=ycbcr -r reference_video.avi video1.avi video2.avi video3.avi
+
+QPSNR.rb
+========
+
+This is a helper ruby script that works like qpsnr but it generates a nice html
+page with a nice SSIM or PSNR graph were each point has a link to the
+corresponding original and encoded frames.
+
+# Requirements
+
+You must have ruby 1.9.2 installed in your system. In Ubuntu the following
+command should suffice:
+
+    sudo apt-get install ruby1.9.1
+
+For this script to work you must have the qpsnr binary and the scripts directory
+that comes with the source in the same folder. This ruby script depends on these
+files to generate the html output.
+
+# Usage
+
+Similar to qpsnr with some small differences:
+
+ * The -J flag is set on by default.
+ * The --aopts flag is replaced by two new flags (-colorspace, -bs).
+ * You cannot use average analyzers (avg_ssim or avg_psnr).
+
+    /path/to/qpsnr.rb -m 400 -r ref.avi out1.mp4 out2.mp4 ... outN.mp4
+
+# HTML Interaction
+
+The resulting html page has the same name of the reference clip with the
+extension changed to html. If you open this html file in a browser you should
+see:
+
+  * The first frame of the original clip.
+  * The first frame of the first input (encoded) clip.
+  * A flot graph of the SSIM or PSNR depending on what you used.
+
+You can click any item in the graph and the corresponding encoded frame will be
+replaced. Also you may use the left and right arrows on the keyboard to navigate
+the points in the graph. The up and down arrow allows to select different input
+(encoded) clips in case you had more than one.
+
+# Warnings
+
+Depending on the size of the media files you are comparing this tool may take a
+long time and huge amounts of hard disk. This tool will output a JPEG image for
+each frame of each input and reference clip on disk so do not run this on large
+files on your ultrabook 160GB SSD harddisk.
+
+Please note that the displayed frames are JPEG encoded and scaled so they are
+not exactly the same frames used for calculating the SSIM or PSNR values. I did
+not made this viewer to check encoding quality visually, instead I did this to
+check that the frames being compared are actually the same frame.
---
+++ qpsnr.rb
@@ -0,0 +1,335 @@
+#!/usr/bin/env ruby1.9.1
+# encoding: UTF-8
+
+DATA=<<-END
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>{{SOURCE}}</title>
+    <script language="javascript" type="text/javascript" src="./scripts/jquery.js"></script>
+    <script language="javascript" type="text/javascript" src="./scripts/jquery.flot.js"></script>
+    <script language="javascript" type="text/javascript" src="./scripts/jquery.flot.crosshair.js"></script>
+    <style type="text/css">
+      #placeholder {
+        width: 1700px;
+        height: 300px;
+      }
+      #imgcontainer {
+        width: 90%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      #original {
+       background: grey;
+       float: left;
+       margin-left: auto;
+       margin-right: 2px;
+       width: 40%;
+      }
+      #original img {
+        width:100%;
+      }
+      #original_name {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      #encoded {
+       background: grey;
+       float: left;
+       margin-right: auto;
+       margin-left: 2px;
+       width: 40%;
+      }
+      #encoded img {
+        width:100%;
+      }
+      #encoded_name {
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      #controls {
+       clear: both;
+       width: 30%;
+       margin-left: auto;
+       margin-right: auto;
+      }
+
+      .arrow-up {
+        	width: 0;
+        	height: 0;
+        	border-left: 5px solid transparent;
+        	border-right: 5px solid transparent;
+
+        	border-bottom: 5px solid black;
+      }
+
+      .arrow-down {
+        	width: 0;
+        	height: 0;
+        	border-left: 20px solid transparent;
+        	border-right: 20px solid transparent;
+
+        	border-top: 20px solid #f00;
+      }
+
+      .arrow-right {
+        	width: 0;
+        	height: 0;
+        	border-top: 60px solid transparent;
+        	border-bottom: 60px solid transparent;
+         
+        	border-left: 60px solid green;
+          float: right;
+          width: 60px;
+      }
+  
+      .arrow-left {
+        	width: 0;
+        	height: 0;
+        	border-top: 60px solid transparent;
+        	border-bottom: 60px solid transparent; 
+
+        	border-right:60px solid blue;
+          float: left;
+          width: 60px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="imgcontainer">
+      <div id="original">
+        <img id="original_img" />
+        <div id="original_name">{SOURCE}</div>
+      </div>
+      <div id="encoded">
+        <img id="encoded_img" />
+        <div id="encoded_name" />
+      </div>
+    </div>
+    <div id="controls">
+    </div>
+    <div id="placeholder"></div>
+
+    <script type="text/javascript">
+      $(function() {
+          var current_value = {VALUE};
+          var current_point = 1;
+          var current_item = 0;
+          var max_point = {MAX};
+          var items = [ {ITEMS} ];
+
+          function zeroFill(number, width) {
+            width -= number.toString().length;
+            if ( width > 0 ) {
+              return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+            }
+            return number + "";
+          }
+
+          var plot = $.plot($("#placeholder"), 
+                [ {DATA} ], {
+                series: {
+                  lines: { show: true, fill: false },
+                  points: { show: true, fill: false }
+                },
+                grid: { hoverable: true, clickable: true },
+                crosshair: { mode: "x" }
+            });
+
+          var data = plot.getData();
+          console.log(data);
+          var previousPoint = null;
+
+          function page(number, item, current_value) {
+            $("#original_img").attr("src", '{BASENAME}/{SOURCE}' + '.' + zeroFill(number, 7) + '.jpeg');
+            $("#encoded_img").attr("src", '{BASENAME}/' + items[item] + '.' + zeroFill(number, 7) + '.jpeg');
+            ssim = data[current_item].data[current_point - 1][1];
+            $("#encoded_name").text(items[item] + ' FRAME: ' + current_point + ' SSIM: ' + ssim );
+            plot.unhighlight();
+            plot.highlight(current_item, current_point - 1);
+            plot.lockCrosshair({ x: data[current_item].data[current_point - 1][0], y: data[current_item].data[current_point - 1][1] });
+            console.log("Current : " + ' ' + items[item] + ' FRAME: ' + current_point.toString() + ' SSIM: ' + ssim );
+          }
+
+          $("#placeholder").bind("plothover", function(event, pos, item) {
+            if(item) {
+            }
+          });
+
+          $("#placeholder").bind('plotclick', function(event, pos, item) {
+            if(item) {
+              //alert("Click " + item.datapoint[0].toFixed(0).toString() + " " + item.datapoint[1].toFixed(2).toString());
+              current_point = parseInt(item.datapoint[0].toFixed(0));
+              current_item = items.indexOf(item.series.label);
+              page(current_point, current_item, current_value);
+            }
+          });
+
+          page(1, 0);
+
+          $("#prev").bind("click", function() {
+            if( current > 0 ) {
+              current = current - 1;
+              page(current);
+            }
+          });
+
+          $("#next").bind("click", function() {
+            current = current + 1;
+            page(current);
+          });
+
+          $("body").keydown(function(event) {
+            if (event.which == 37) {
+              if(current_point > 1) {
+                current_point = current_point - 1;
+              } else {
+                current_point = max_point;
+              }
+              page(current_point, current_item);
+            } else if(event.which == 39) {
+              if((current_point + 1) <= max_point) {
+                current_point = current_point + 1;
+              } else {
+                current_point = 1;
+              }
+              page(current_point, current_item);
+            } else if(event.which == 38) {
+              if(current_item <= 0) {
+                current_item = items.length - 1;
+              } else {
+                current_item = current_item - 1;
+              }
+              page(current_point, current_item);
+            } else if(event.which == 40) {
+              current_item = (current_item + 1)%items.length;
+              page(current_point, current_item);
+            }
+            console.log(event.which);
+          });
+      });
+    </script>
+  </body>
+
+</html>
+END
+
+require "optparse"
+require "ostruct"
+require "fileutils"
+
+# Utility method to change working directory
+def cwd dir
+  original_dir = Dir.pwd
+  begin
+    puts "Creating #{dir} directory"
+    FileUtils.mkdir_p(dir)
+    Dir.chdir dir
+    yield
+  ensure
+    Dir.chdir original_dir
+  end
+end
+
+
+options = OpenStruct.new
+options.library = {}
+options.library[:ana] = :ssim
+options.library[:log] = 3
+options.library[:color] = :rgb
+options.library[:bs] = 8
+options.inplace = false
+options.encoding = "utf8"
+options.transfer_type = :auto
+options.verbose = false
+
+opts = OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename __FILE__} [options] -r REFERENCE  INPUT1 INPUT2 ..."
+  opts.separator ""
+  opts.separator "Specific options: "
+  opts.on("-r", "--reference REF", "Reference REF") { |lib| options.library[:ref] = lib }
+  opts.on("-m", "--max-frames [MAXFRAMES]", "set max frames to process before quit") { |lib| options.library[:max] = lib }
+  opts.on("-s", "--skip-frames [SKIPFRAMES]", "skip n initial frames") { |lib| options.library[:skip] = lib }
+  opts.on("-a", "--analyzer ANALYZER", [:psnr, :ssim], "psnr, ssim (default)") { |lib| options.library[:ana] = lib }
+  opts.on("-l", "--loglevel LEVEL", [0, 1, 2, 3, 4], "0: no log, 1: errors, 2: warnings, 3: info (default), 4: debug") { |lib| options.library[:ana] = lib }
+  opts.on("-c", "--colorspace [SPACE]", [:rgb, :hsi, :ycbcr, :y], "color space to use (PSNR only): rgb, hsi, ycbcr or y (default rgb)") { |lib| options.library[:color] = lib }
+  opts.on("-bs", "--block-size BS", "block size used in SSIM analysis (default 8)") { |lib| options.library[:bs] = lib }
+end
+
+opts.parse!(ARGV)
+
+if options.library[:ref].nil?
+  puts "Missing reference clip (-r) argument"
+  exit -1
+end
+
+if ARGV.empty?
+  puts "Missing input clips"
+end
+
+files = ARGV.join(" ")
+ARGV.each do |f|
+  if ! File.exists?(f) or ! File.readable?(f)
+    puts "File #{f} does not exists or is not readable"
+    exit
+  end
+end
+
+#qpsnr_cmd = "/usr/bin/qpsnr -a ssim -I -m 10 -r #{ARGV.join(" ")}"
+qpsnr_cmd = "#{File.dirname(__FILE__)}/qpsnr "
+qpsnr_cmd += "-a #{options.library[:ana]} -J -G "
+qpsnr_cmd += "-m #{options.library[:max]} " if options.library[:max].to_i > 0
+qpsnr_cmd += "-s #{options.library[:skip]} " if options.library[:skip].to_i > 0
+qpsnr_cmd += "-o colorspace=#{options.library[:color]}:blocksize=#{options.library[:bs]} "
+qpsnr_cmd += "-r #{options.library[:ref]} "
+qpsnr_cmd += "#{ARGV.join(" ")}"
+
+#puts "== Running qpsnr on input files"
+puts qpsnr_cmd
+
+data = []
+links = ""
+
+result = ""
+dirname = File.dirname options.library[:ref]
+extname = File.extname options.library[:ref]
+basename = File.basename options.library[:ref], extname
+
+cwd basename do
+  result = `#{qpsnr_cmd}`
+  result.split.each do |line|
+      values = line.split(",")
+      next if values[0] == "Sample"
+      ARGV.size.times do |col|
+        data[col] ||= []
+        data[col] << [values[0].to_i, values[col + 1].to_f]
+      end
+  end
+end
+
+File.open("#{basename}.dat", "w") { |fp| fp << result }
+
+max = data[0].size
+val = data[0][0]
+# Convert data to a string for flot
+data = data.each_with_index.map do |x, i|
+  "{ data: #{x.to_s}, label: '#{File.basename(ARGV[i])}' }"
+end.join(",")
+
+FileUtils.mkdir_p("scripts")
+FileUtils.cp(File.join(File.dirname(__FILE__), "scripts", "jquery.js"), "scripts/")
+FileUtils.cp(File.join(File.dirname(__FILE__), "scripts", "jquery.flot.js"), "scripts/")
+FileUtils.cp(File.join(File.dirname(__FILE__), "scripts", "jquery.flot.crosshair.js"), "scripts/")
+
+File.open("#{basename}.html", "w") do |fp|
+ fp << DATA.gsub("{SOURCE}", "#{basename}#{extname}")
+         .gsub("{BASENAME}", basename)
+         .gsub("{MAX}", max.to_s )
+         .gsub("{VALUE}", val.to_s )
+         .gsub("{ITEMS}", ARGV.map { |a| "'#{File.basename(a)}'" }.join(",").to_s)
+         .gsub("{DATA}", data.to_s)
+end
